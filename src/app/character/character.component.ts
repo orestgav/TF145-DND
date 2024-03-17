697d511e154a279fcec33cb2b1e35581cd29d1c5
AMCBRIDGE\ohavryliak <ohavryliak@amcbridge.com>
2024-03-17 10:21:11 +0200

    new

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -8,34 +8,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class CharacterComponent implements OnInit {
   @Input() id: string = '';
+  @Input() name: string = '';
+  @Output() hideChar = new EventEmitter<void>();
+  @Output() moneyEarned = new EventEmitter<number>();
+  public allModifiers: any[] = [];
+
+  public str: number = 0;
+  public dex: number = 0;
+  public con: number = 0;
+  public int: number = 0;
+  public wis: number = 0;
+  public chr: number = 0;
+
   public char: any;
   public loaded: boolean = false;
   public equipment: any[] = [];
   public backpack: any[] = [];
   public chest: any[] = [];
   public bag: any[] = [];
+  public money: number = 0;
+  public maxWeight: number = 0;
+
+  public maxHP: number = 0;
+  public curHP: number = 0;
+  public level: number = 6;
 
   constructor(private http: HttpClient) { }
 
   public roundOne(value: number): number {
     return Math.round(value * 10) / 10; 
   }
+
   public toKg(value: number): number {
     return Math.round(value * 0.45); 
   }
 
-  public level(): number {
-    return 6;
-  }
-
-  public money(): number {
-
-    return Math.round(this.char.currencies.cp/100 + this.char.currencies.sp/10 + this.char.currencies.ep/2 + this.char.currencies.gp + this.char.currencies.pp*10);
-  }
-
-  public maxWeight(): number {
-    return this.char.stats[0].value * 15; 
-  }
 
   public allWeight(items: any[]): number {
 
@@ -45,33 +52,19 @@ export class CharacterComponent implements OnInit {
       sum += item.definition.weight * item.quantity;
     });
 
-    return  Math.round(sum);
-  }
-
-  public maxHP(): number {
-    let hp = this.char.baseHitPoints + this.char.temporaryHitPoints + (Math.round((this.char.stats[2].value - 10)/2) * this.level());
-
-    if (this.char.feats.find((f: { definition: any; }) => f.definition.name == 'Tough')) {
-      hp += 2 *  this.level();
-    }
-
-    return hp;
-  }
-
-  public curHP(): number {
-    return this.maxHP() - this.char.removedHitPoints;
+    return Math.round(sum);
   }
 
   public valHP(): number {
-    return this.curHP() * (100 / this.maxHP());
+    return this.curHP * (100 / this.maxHP);
   }
 
+  public weightPers(): number {
+    return Math.round((this.allWeight(this.equipment) + this. allWeight(this.backpack))/(this.maxWeight / 100)  );
+  }
+  
   ngOnInit(): void {
-   
-
     this.loadData();
-
-
   }
 
   public loadData(): void {
@@ -81,10 +74,41 @@ export class CharacterComponent implements OnInit {
     req.subscribe(result => {
       if (result.success) {
         this.char = result.data;
-        console.log( this.char );
+        console.log(this.char.name);
+        console.log(this.char);
+
         this.char.inventory = this.modifyItems(this.char.inventory)
+        this.allModifiers.push(...this.char.modifiers.class);
+        this.allModifiers.push(...this.char.modifiers.feat);
+        this.allModifiers.push(...this.char.modifiers.item);
+        this.allModifiers.push(...this.char.modifiers.race);
 
 
+        // stats 
+        this.str = this.char.stats[0].value + this.bonus('strength-score');
+        this.dex = this.char.stats[1].value + this.bonus('dexterity-score');
+        this.con = this.char.stats[2].value + this.bonus('constitution-score');
+        this.int = this.char.stats[3].value + this.bonus('intelligence-score');
+        this.wis = this.char.stats[4].value + this.bonus('wisdom-score');
+        this.chr = this.char.stats[5].value + this.bonus('charisma-score');
+        
+        // weight 
+        this.maxWeight = this.str * 15; 
+
+        // money
+        this.money = Math.round(this.char.currencies.cp/100 + this.char.currencies.sp/10 + this.char.currencies.ep/2 + this.char.currencies.gp + this.char.currencies.pp*10);
+        this.moneyEarned.emit(this.money);
+
+        // HP
+        let hp = this.char.baseHitPoints + this.char.temporaryHitPoints + (Math.round((this.con - 10)/2) * this.level);
+
+        if (this.char.feats.find((f: { definition: any; }) => f.definition.name == 'Tough')) {
+          hp += 2 *  this.level;
+        }
+        this.maxHP = hp;
+        this.curHP = this.maxHP - this.char.removedHitPoints;
+
+        // equipment
         let equipmentId = this.char.id;
         let bag = this.char.inventory.find((i: { definition: { name: string; }; }) => i.definition.name == 'Bag of Holding');
         let bagId = bag ? bag.id : 0;
@@ -117,4 +141,18 @@ export class CharacterComponent implements OnInit {
     return items;
   }
 
+  public bonus(type: string): number {
+    let bonus = 0;
+    this.allModifiers.forEach(m => {
+      if (m.subType == type) {
+        bonus += m.fixedValue;
+      }
+    });
+
+    return bonus;
+  }
+
+  public hide(): void {
+    this.hideChar.emit();
+  }
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,14 +7,83 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  public chars = ['92925939',  '109247308', '100187035', '110500470', '116521580', '114321107', '92925793',  ];
-  //'112814254', '90458811', '93647500' 
-  constructor() { }
+  public info = [
+    { id:'112814254', name: 'Aris', money: 0 },
+    { id:'114321107', name: 'Boratello', money: 0 },
+    { id:'100187035', name: 'Corvo', money: 0 },
+    { id:'93647500', name: 'Ihorko', money: 0 },
+    { id:'90458811', name: 'Marianna', money: 0 },
+    { id:'92925793', name: 'Nadine', money: 0 },
+    { id:'110500470', name: 'Olgert', money: 0 },
+    { id:'109247308', name: 'Gosling', money: 0 },
+    { id:'92925939', name: 'Fishkinson', money: 0 },
+    { id:'116521580', name: 'Yurulf', money: 0 },
+  ];
+  
+  public chars:string[] = [];
+  public hiden:string[] = [];
+
+  public sum(): number {
+    let sum = 0;
+
+    this.info.forEach(i => {
+      if (this.chars.includes(i.id)) {
+        sum += i.money;
+      }
+    });
+
+    return sum;
+  }
+
+  constructor() { 
+    this.info.forEach(i => {
+      if (!window.localStorage.getItem(i.id)) {
+        this.chars.push(i.id);
+      } else {
+        this.hiden.push(i.id);
+      }
+
+    });
+  }
 
   ngOnInit(): void {
 
+  }
 
+  public hide(id: string): void {
+    const index = this.chars.indexOf(id);
+    if (index > -1) { 
+      this.chars.splice(index, 1);
+      window.localStorage.setItem(id, 'true');
+      this.hiden.push(id);
+    }
+  }
 
+  public unhide(id: string): void {
+    const index = this.hiden.indexOf(id);
+    if (index > -1) { 
+      this.hiden.splice(index, 1);
+      window.localStorage.removeItem(id);
+      this.chars.push(id);
+    }
+  }
+
+  public money(id: string, sum: number): void {
+    const char = this.info.find(i => i.id == id);
+
+    if (char) {
+      char.money = sum;
+    }
+  }
+
+  public nameFromId(id: string): string {
+    const item = this.info.find(i => i.id == id);
+
+    if (item) {
+      return item.name;
+    }
+
+    return '';
   }
 
 }
